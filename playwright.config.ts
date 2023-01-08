e0199f1d3b4ecea87679b66883ac872ac2dccdbd
Diego <cortesroadiegoalejandro@gmail.com>
2023-01-07 22:45:53 -0500

    main: Added and setting up .env file

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,15 +26,16 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: "html",
-  reporter: "./tests/utils/playwright-reporter.ts",
+  reporter: process.env.CI ? './tests/utils/playwright-reporter.ts' : 'html',
+  //reporter: "./tests/utils/playwright-reporter.ts",
+  //reporter: [['list', { printSteps: true }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: true,
+    headless: false,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
